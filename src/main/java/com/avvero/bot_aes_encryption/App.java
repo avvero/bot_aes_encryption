9423727808ad749771f41f80d3c12e3391d99351
Avvero <avvero@mail.ru>
2017-05-24 22:28:49 +0700

    Update App.java

--- src/main/java/com/avvero/bot_aes_encryption/App.java
+++ src/main/java/com/avvero/bot_aes_encryption/App.java
@@ -1,12 +1,12 @@
 package com.avvero.bot_aes_encryption;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by fxdev-belyaev-ay on 03.11.16.
  */
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
 
     public static void main(String args[]) throws Throwable {
