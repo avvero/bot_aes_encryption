2bcf666e0ee1c6a77631ae0f04b70d179e60445f
belyaev-ay <anton.belyaev@fxclub.com>
2017-05-25 16:59:33 +0600

    base64

--- src/main/java/com/avvero/bot_aes_encryption/service/AesEncryptionService.java
+++ src/main/java/com/avvero/bot_aes_encryption/service/AesEncryptionService.java
@@ -1,6 +1,7 @@
 package com.avvero.bot_aes_encryption.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -29,11 +31,15 @@ public class AesEncryptionService {
         try {
             Cipher cipher = Cipher.getInstance(schema, "BC");
             cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(), "AES"));
-            return new String(cipher.doFinal(value.getBytes()));
+            return encodeBase64(cipher.doFinal(value.getBytes()));
         } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             log.error(e.getMessage(), e);
             return e.getMessage();
         }
+    }
+
+    public static String encodeBase64(byte[] bytes) {
+        return new String(Base64.encodeBase64(bytes));
     }
 
 }
